43537c0c857e694f57a602d47b2979b60835ecdb
Ganidu Chalinda <ganiduchalinda@gmail.com>
2025-08-30 20:53:37 +0530

    create order model file 👌

---
+++ models/order.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const orderSchema = mongoose.Schema({
+    orderID : {
+        type : String,
+        unique : true
+    },
+    email : {
+        type : String,
+        required : true
+    }, 
+    orderName : {
+        type : String,
+        required : true
+    },
+    orderItems : [
+        {
+            name : {
+                type : String,
+                required : true
+            },
+            price : {
+                type : String,
+                required : true
+            },quentity : { 
+                type : Number,
+                required : true
+            },
+            image : {
+                type : String,
+                required : true
+            }
+
+        }
+    ],
+    date : { 
+        type : Date,
+        default : Date.now
+    },
+    paymentId : { 
+        type : String
+    },
+    status : {
+        type : String,
+        default : "Preparing"
+    },
+    notes : {
+        type : String
+    },
+    name : {
+        type : String,
+        required : true
+    },
+    address : {
+        type : String,
+        required : true
+    },
+    phone : {
+        type : Number,
+        required : true
+    }
+})
+
+const order = mongoose.model(orderSchema)
+
+export default order;
