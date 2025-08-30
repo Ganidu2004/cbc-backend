d9262827abc7f46a02f09effacb6184e6b9ebb52
Ganidu Chalinda <ganiduchalinda@gmail.com>
2025-08-30 11:05:55 +0530

    create product controller👌

---
+++ controllers/productController.js
@@ -0,0 +1,27 @@
+import { json } from "body-parser";
+import product from "../models/product.js";
+import { isAdmin } from "./userController.js";
+import product from "../models/product.js";
+
+export function createProduct(req,res){
+    if(!isAdmin(req)){
+        res,json({
+            message : "Please login as administrator to add product"
+        })
+        return
+    }
+
+    const newProductData = req.body
+
+    const product = new product(newProductData)
+
+    product.save().then(()=>{
+        res.json({
+            message : "Product created"
+        })
+    }).catch((error)=>{
+        res.json({
+            message : "Product nit created"
+        })
+    })
+}
