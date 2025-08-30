1f4870c0b4ba3c5ac44bd16015ad67518101e767
Ganidu Chalinda <ganiduchalinda@gmail.com>
2025-08-30 21:34:40 +0530

    create a order controll function👍

---
+++ controllers/orderController.js
@@ -0,0 +1,53 @@
+import { cache } from "react";
+import order from "../models/order.js";
+import { isCustomer } from "./userController.js";
+
+export async function createOrder(req,res){
+
+    if(!isCustomer){
+        res.json({
+            message : "Please login as customer to create orders"
+        })
+    }
+    //take the latest product Id
+    try{
+        const latestOrder = await order.find().sort({date : -1}).limit(1)
+
+        let orderId;
+
+        if(latestOrder.length == 0){
+
+            orderId = "CBC0001"
+
+        }else{
+
+            const currentOrderId = latestOrder[0].orderId
+
+            const numberString = currentOrderId.replace("CBC","")
+
+            const number = parseInt(numberString)
+
+            const newNumber = (number + 1).toString().padStart(4,"0");
+
+            orderId = "CBC" + newNumber
+
+        }
+
+        const newOrderData = req.body
+        newOrderData.orderId = orderId
+        newOrderData.email = req.user.email
+
+        const newOrder = new order(newOrderData)
+
+        await order.save()
+
+        res.json({
+            message : "Order created"
+        })
+
+    }catch(error){
+        res.status(500).json({
+            message : error.message
+        })
+    }
+}
